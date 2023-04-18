8155539d457b963de5c7ecc7595afae33a89b3c0
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-04-18 15:49:56 +0200

    more

--- egui-inspect-derive/src/lib.rs
+++ egui-inspect-derive/src/lib.rs
@@ -4,7 +4,7 @@ use quote::quote;
 #[proc_macro_derive(Inspect)]
 pub fn derive_inspect(input: TokenStream) -> TokenStream {
     let expanded = quote! {
-        impl Inspect for GameState {
+        impl GameState {
             fn inspect_mut(&mut self) {
                 output_mut(|o| o.copied_text = "".into());
                 output_mut(|o| o.copied_text = format!("{:?}", self.tile_db));
