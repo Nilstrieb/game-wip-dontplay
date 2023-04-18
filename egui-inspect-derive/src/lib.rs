80206ebcd384af7fb2d90d32822fe18883b5807a
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-04-18 21:05:41 +0200

    more

--- egui-inspect-derive/src/lib.rs
+++ egui-inspect-derive/src/lib.rs
@@ -2,14 +2,14 @@ use proc_macro::TokenStream;
 use quote::quote;
 
 #[proc_macro_derive(Inspect)]
-pub fn derive_inspect(input: TokenStream) -> TokenStream {
-    let expanded = quote! {
+pub fn derive_inspect(_: TokenStream) -> TokenStream {
+    quote! {
         impl GameState {
             fn inspect_mut(&mut self) {
                 output_mut(|o| o.copied_text = "".into());
                 output_mut(|o| o.copied_text = format!("{:?}", self.tile_db));
             }
         }
-    };
-    proc_macro::TokenStream::from(expanded)
+    }
+    .into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate alloc;
-
 pub struct TileDef {
     pub layer: (),
     #[cfg(second)]
