785078098e2eb4bb6d50c39a9bd7cbd6a0a1cc17
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-04-18 12:09:20 +0200

    more

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ mod tiles;
 pub(crate) struct App {
     pub(crate) game: GameState,
     pub(crate) sf_egui: SfEgui,
-    /// Integer scale for rendering the game
-    pub(crate) scale: u8,
 }
 impl App {
     pub(crate) fn new() -> Self {
@@ -20,12 +18,12 @@ fn main() {
     let mut app = App::new();
     app.sf_egui
         .do_frame(|ctx| {
-            do_debug_ui(ctx, &mut app.game, &mut app.scale);
+            do_debug_ui(ctx, &mut app.game);
         })
         .unwrap();
 }
 
-pub(crate) fn do_debug_ui(ctx: &egui::Context, mut game: &mut GameState, mut scale: &mut u8) {
+pub(crate) fn do_debug_ui(ctx: &egui::Context, mut game: &mut GameState) {
     egui::Window::new("Debug (F12)").show(ctx, |ui| {
         egui::ScrollArea::both().show(ui, |ui| {
             let mut id_source = 0;
