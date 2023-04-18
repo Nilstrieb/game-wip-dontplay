39f436e014ca8f3a477803a04fd4824600cf2882
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-04-18 12:23:25 +0200

    more

--- src/tiles.rs
+++ src/tiles.rs
@@ -1,35 +1,27 @@
 use std::{fmt::Debug, marker::PhantomData};
 
 use egui_inspect::{derive::Inspect, Inspect};
-use serde::{Deserialize, Serialize};
 
 #[derive(Inspect)]
-pub struct TileDef
-where
-{
+pub struct TileDef {
     pub layer: (),
     //ADD pub blend_graphic: String,
 }
 
-impl Debug for TileDef
-where
-{
+impl Debug for TileDef {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         loop {}
     }
 }
 
-impl Default for TileDef<>
-where
-{
-    fn default() -> Self {
-        loop {}
-    }
-}
-
-
 // this is actually used
-#[derive(Debug, Inspect)]
+#[derive(Inspect)]
 pub struct TileDb {
     unknown_bg: TileDef,
+}
+
+impl Debug for TileDb {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        loop {}
+    }
 }
--- tiles.rs
+++ tiles.rs
@@ -3,32 +3,25 @@ use std::{fmt::Debug, marker::PhantomData};
 use egui_inspect::{derive::Inspect, Inspect};
 
 #[derive(Inspect)]
-pub struct TileDef
-where
-{
+pub struct TileDef {
     pub layer: (),
     //ADD pub blend_graphic: String,
 }
 
-impl Debug for TileDef
-where
-{
+impl Debug for TileDef {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         loop {}
     }
 }
 
-impl Default for TileDef<>
-where
-{
-    fn default() -> Self {
-        loop {}
-    }
-}
-
-
 // this is actually used
-#[derive(Debug, Inspect)]
+#[derive(Inspect)]
 pub struct TileDb {
     unknown_bg: TileDef,
+}
+
+impl Debug for TileDb {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        loop {}
+    }
 }
