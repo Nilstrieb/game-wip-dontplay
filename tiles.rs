41efc35b390a97163637b43e40f7f7489d40cd7f
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-04-18 12:22:19 +0200

    more

--- tiles.rs
+++ tiles.rs
@@ -3,36 +3,12 @@ use std::{fmt::Debug, marker::PhantomData};
 use egui_inspect::{derive::Inspect, Inspect};
 use serde::{Deserialize, Serialize};
 
-#[derive(Inspect, PartialEq, Eq)]
-pub struct TileId<Layer>(pub u16, PhantomData<Layer>);
-
-impl<Layer> Copy for TileId<Layer> {}
-impl<Layer> Clone for TileId<Layer> {
-    fn clone(&self) -> Self {
-        Self(self.0, PhantomData)
-    }
-}
-impl<Layer> Debug for TileId<Layer> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        loop {}
-    }
-}
-
-
 #[derive(Serialize, Deserialize, Inspect)]
 pub struct TileDef
 where
 {
     pub layer: (),
     //ADD pub blend_graphic: String,
-}
-
-#[derive(Debug, Inspect, Default, Serialize, Deserialize)]
-pub struct MidDef {
-    /// Platform behavior: Horizontally passable, vertically passable upwards
-    pub platform: bool,
-    /// Collision bounding box
-    pub bb: Option<TileBb>,
 }
 
 impl Debug for TileDef
